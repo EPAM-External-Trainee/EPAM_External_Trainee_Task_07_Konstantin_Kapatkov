0a0a1a775443a5252c182312f171f21fde68bcbe
Константин <kotkat.lv@gmail.com>
2020-09-07 14:06:47 +0300

    commit

--- BLL/Reports/Models/ReportData/AssessmentDynamicsReportData.cs
+++ BLL/Reports/Models/ReportData/AssessmentDynamicsReportData.cs
@@ -1,6 +1,7 @@
 ﻿using BLL.Reports.Interfaces.GroupSessionResultReport;
 using BLL.Reports.Structs.ExcelTableRawViews.DynamicChangesInAverageMark;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BLL.Reports.Structs.ReportData
 {
@@ -19,5 +20,15 @@ namespace BLL.Reports.Structs.ReportData
         public IEnumerable<AssessmentDynamicsTableRowView> TableRowViews { get; set; }
 
         public IEnumerable<string> AcademicYears { get; set; }
+
+        public override bool Equals(object obj) => obj is AssessmentDynamicsReportData data && TableRowViews.SequenceEqual(data.TableRowViews) && AcademicYears.SequenceEqual(data.AcademicYears);
+
+        public override int GetHashCode()
+        {
+            int hashCode = -294578147;
+            hashCode = hashCode * -1521134295 + TableRowViews.GetHashCode();
+            hashCode = hashCode * -1521134295 + AcademicYears.GetHashCode();
+            return hashCode;
+        }
     }
 }
