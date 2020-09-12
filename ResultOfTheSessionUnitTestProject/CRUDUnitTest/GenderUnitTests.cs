78dbb93c026884677bdc1a17f2cca8ce1c0afcd5
Константин <kotkat.lv@gmail.com>
2020-09-12 19:32:57 +0300

    commit

--- ResultOfTheSessionUnitTestProject/CRUDUnitTest/GenderUnitTests.cs
+++ ResultOfTheSessionUnitTestProject/CRUDUnitTest/GenderUnitTests.cs
@@ -5,7 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ResultOfTheSessionUnitTestProject
 {
     [TestClass]
-    public class UnitTest1
+    public class GenderUnitTests
     {
         private DaoFactory daoFactory = DaoFactory.GetInstance(@"Data Source=KONSTANTINPC\SQLEXPRESS; Initial Catalog=ResultSession; Integrated Security=true;");
 
