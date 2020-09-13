47792fb29df7f02ca5b204ee4b497f61bec03bf7
Константин <kotkat.lv@gmail.com>
2020-09-13 20:28:03 +0300

    commit

--- ResultOfTheSessionUnitTestProject/ReportsUnitTest/ReportsUnitTestData.cs
+++ ResultOfTheSessionUnitTestProject/ReportsUnitTest/ReportsUnitTestData.cs
@@ -3,10 +3,13 @@
     /// <summary>Class describes common data for testing reports classes</summary>
     public abstract class ReportsUnitTestData
     {
+        /// <summary>SQL Server connection string</summary>
         protected const string ConnectionString = @"Data Source=KONSTANTINPC\SQLEXPRESS; Initial Catalog=ResultSession; Integrated Security=true;";
 
+        /// <summary>Path to session result report excel file</summary>
         protected const string PathToSessionResultReportExcelFile = @"..\..\..\ResultOfTheSessionUnitTestProject\ReportsUnitTest\Resources\SessionResultReport.xlsx";
 
+        /// <summary>Path to group session result report excel file</summary>
         protected const string PathToGroupSessionResultReportExcelFile = @"..\..\..\ResultOfTheSessionUnitTestProject\ReportsUnitTest\Resources\GroupSessionResultReport.xlsx";
     }
 }
